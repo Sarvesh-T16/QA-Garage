5f6c8d56bb6e1c111465114ae5c693a0d7be42fa
jacob <jacobcurtis786@gmail.com>
2024-10-29 11:22:29 +0000

    Garage classes etc.

---
+++ QA_Garage/src/Garage.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Garage {
+    private List<Vehicle> vehicles = new ArrayList<>();
+
+    public Garage(List<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+
+    }
+
+    public Garage() {
+    }
+
+    public void addVehicle(Vehicle vehicle){
+        vehicles.add(vehicle);
+        System.out.println("Vehicle added "+vehicle);
+
+    }
+
+    public void removeVehicle(int id){ //Removes by id
+        for (Vehicle vehicle: vehicles){
+            if (vehicle.getId()==id) {
+                vehicles.remove(vehicle);
+            }
+
+        }
+    }
+
+    public void removeType(String type) {
+        for (Vehicle vehicle: vehicles) {
+            if (vehicle.getClass().getName() == type){ //Checking class type of vehicle and comparing to type argument
+                vehicles.remove(vehicle);
+            }
+        }
+    }
+
+    public void displayVehicles(){
+        for (Vehicle vehicle: vehicles) {
+            System.out.println(vehicle);
+        }
+    }
+
+    public void emptyGarage(){
+        vehicles.clear();
+    }
+
+    public double calculateBill(){
+        double bill = 0;
+        for (Vehicle vehicle: vehicles){
+            switch (vehicle.getClass().getName()){
+                case "Car":
+                    bill+=100;
+                    break;
+                case "Motorbike":
+                    bill+=50;
+                    break;
+                case "Tractor":
+                    bill+=200;
+                    break;
+                }
+            }
+        return bill;
+    }
+}
--- QA_Garage/src/Runner.java
+++ QA_Garage/src/Runner.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Runner {
     public static void main(String[] args) {
         Car car1 = new Car(2004,"Ford",4,1,5,true);
+        Car car2 = new Car(2004,"Ford",4,4,5,true);
         Motorbike bike1 = new Motorbike(2021,"Yamaha",2,2,350,"Sports");
         Tractor tractor1 = new Tractor(1984,"Rolls Royce",4,3,24.0f,4.0f);
 
@@ -8,5 +12,17 @@ public class Runner {
         System.out.println(bike1);
         System.out.println(tractor1);
 
+//        List<Vehicle> vehicleList = new ArrayList<>();
+//
+//
+        Garage garage1 = new Garage();
+        garage1.addVehicle(car1);
+        garage1.addVehicle(tractor1);
+        garage1.addVehicle(bike1);
+        garage1.removeVehicle(3);
+        garage1.removeType("Car");
+//        garage1.emptyGarage();
+        garage1.displayVehicles();
+        System.out.println(garage1.calculateBill());
     }
 }
